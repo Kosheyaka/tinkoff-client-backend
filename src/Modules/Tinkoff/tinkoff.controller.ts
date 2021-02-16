71471dc955f27f3db944796987e974656a0a3bff
Dmitry Volkov <koshey0@inbox.ru>
2021-02-16 06:03:48 +0300

    Tinkoff Api sandbox/register method implemented

--- src/Modules/Tinkoff/tinkoff.controller.ts
+++ src/Modules/Tinkoff/tinkoff.controller.ts
@@ -1,11 +1,33 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { SandboxRegisterRequest, SandboxRegisterResponse } from "@tinkoff/invest-openapi-js-sdk/";
+import axios, { AxiosRequestConfig } from "axios";
+import { app } from "../../main";
 
-@Controller()
+@Controller('tinkoff')
 export class TinkoffController {
-  constructor() {}
 
-  @Get('hello')
-  getHello(): string {
-    return 'This is TinkoffController.getHello!';
+  private axiosConfig: AxiosRequestConfig = {
+    headers: {
+      authorization: `Bearer ${app.config.tinkoff.sandboxToken}`,
+    }
+  };
+
+  @Post('registerSandbox')
+  async registerSandbox() {
+    const { apiUrlSandbox } = app.config.tinkoff;
+
+    const body: SandboxRegisterRequest = {
+      brokerAccountType: "Tinkoff",
+    };
+
+    let data: SandboxRegisterResponse = null;
+    try {
+      const response = await axios.post(apiUrlSandbox + 'sandbox/register', body, this.axiosConfig);
+      data = response.data;
+    } catch (e) {
+      console.error(e.message);
+    }
+
+    return data;
   }
 }
