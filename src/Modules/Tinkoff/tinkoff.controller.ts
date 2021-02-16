8af23ac7880673305ac66df8101152c74a90099b
Dmitry Volkov <koshey0@inbox.ru>
2021-02-16 06:26:15 +0300

    Tinkoff Api user/accounts method implemented

--- src/Modules/Tinkoff/tinkoff.controller.ts
+++ src/Modules/Tinkoff/tinkoff.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 import { TinkoffService } from "./tinkoff.service";
 
 @Controller('tinkoff')
 export class TinkoffController {
   constructor(private service: TinkoffService) {}
 
+  @Get('userAccounts')
+  async userAccounts() {
+    return this.service.getUserAccounts();
+  }
+
   @Post('registerSandbox')
   async registerSandbox() {
-    return this.service.sandboxRegister();
+    return this.service.postSandboxRegister();
   }
 }
--- src/Modules/Tinkoff/tinkoff.service.ts
+++ src/Modules/Tinkoff/tinkoff.service.ts
@@ -1,24 +1,43 @@
-import { SandboxRegisterRequest, SandboxRegisterResponse } from "@tinkoff/invest-openapi-js-sdk";
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosInstance } from "axios";
+import {
+  BrokerAccountType,
+  UserAccountsResponse,
+  SandboxRegisterResponse,
+} from "@tinkoff/invest-openapi-js-sdk";
 import { app } from "../../main";
 
 export class TinkoffService {
-  private axiosConfig: AxiosRequestConfig = {
-    headers: {
-      authorization: `Bearer ${app.config.tinkoff.sandboxToken}`,
+  constructor() {
+    this.api = axios.create({
+      baseURL: app.config.tinkoff.apiUrl,
+      headers: {
+        authorization: `Bearer ${app.config.tinkoff.secretToken}`,
+      },
+    });
+    this.sandboxApi = axios.create({
+      baseURL: app.config.tinkoff.apiUrlSandbox,
+      headers: {
+        authorization: `Bearer ${app.config.tinkoff.sandboxToken}`,
+      },
+    });
+  }
+
+  private api: AxiosInstance;
+  private sandboxApi: AxiosInstance;
+
+  public postSandboxRegister = async (brokerAccountType: BrokerAccountType = "Tinkoff"): Promise<SandboxRegisterResponse> => {
+    try {
+      const { data } = await this.sandboxApi.post('/sandbox/register', { brokerAccountType });
+      return data as SandboxRegisterResponse;
+    } catch (e) {
+      console.error(e.message);
     }
   };
 
-  public sandboxRegister = async (): Promise<SandboxRegisterResponse> => {
-    const { apiUrlSandbox } = app.config.tinkoff;
-
-    const body: SandboxRegisterRequest = {
-      brokerAccountType: "Tinkoff",
-    };
-
+  public getUserAccounts = async (): Promise<UserAccountsResponse> => {
     try {
-      const { data } = await axios.post(apiUrlSandbox + 'sandbox/register', body, this.axiosConfig);
-      return data as SandboxRegisterResponse;
+      const { data } = await this.sandboxApi.get('/user/accounts');
+      return data as UserAccountsResponse;
     } catch (e) {
       console.error(e.message);
     }
