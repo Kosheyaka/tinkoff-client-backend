7e08779757d149f87070b4cdcacff15dd9ebda3b
Dmitry Volkov <koshey0@inbox.ru>
2021-02-17 05:12:04 +0300

    build fixed

--- src/Modules/Tinkoff/tinkoff.module.ts
+++ src/Modules/Tinkoff/tinkoff.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TinkoffController } from './tinkoff.controller';
 import { TinkoffApi } from "./api/tinkoff.api";
+import { TinkoffSandboxApi } from "./api/tinkoffSandbox.api";
 
 @Module({
   imports: [],
   controllers: [TinkoffController],
-  providers: [TinkoffApi],
+  providers: [TinkoffApi, TinkoffSandboxApi],
 })
 export class TinkoffModule {}
