f707f1f26b708a9b137a0b28c8248c0a41addcf2
Dmitry Volkov <koshey0@inbox.ru>
2021-02-21 17:50:32 +0300

    Tinkoff Api: try-catches removed

--- src/Modules/Tinkoff/api/tinkoff.api.abs.ts
+++ src/Modules/Tinkoff/api/tinkoff.api.abs.ts
@@ -56,79 +56,57 @@ export abstract class TinkoffApiAbstract {
   // Portfolio: Операции с портфелем пользователя
 
   public portfolio = async (brokerAccountId?: string): Promise<PortfolioResponse> => {
-    try {
-      const { data } = await this.api.get('/portfolio', brokerAccountId ? { params: { brokerAccountId }} : null);
-      return data as PortfolioResponse;
-    } catch (e) {
-      console.error(e.message);
-    }
+    const { data } = await this.api.get('/portfolio', brokerAccountId ? { params: { brokerAccountId }} : null);
+    return data as PortfolioResponse;
   };
 
   public portfolioCurrencies = async (brokerAccountId?: string): Promise<PortfolioCurrenciesResponse> => {
-    try {
-      const { data } = await this.api.get('/portfolio/currencies', brokerAccountId ? { params: { brokerAccountId }} : null);
-      return data as PortfolioCurrenciesResponse;
-    } catch (e) {
-      console.error(e.message);
-    }
+    const { data } = await this.api.get('/portfolio/currencies', brokerAccountId ? { params: { brokerAccountId }} : null);
+    return data as PortfolioCurrenciesResponse;
   };
 
   // Market: Получении информации по бумагам
 
+  public marketStocks = async (): Promise<MarketInstrumentListResponse> => {
+    const { data } = await this.api.get('/market/stocks');
+    return data as MarketInstrumentListResponse;
+  };
+
   public marketCandles = async (params: MarketCandlesParams): Promise<CandlesResponse> => {
-    try {
-      const { data } = await this.api.get('/market/candles', { params });
-      return data as CandlesResponse;
-    } catch (e) {
-      console.error(e.message);
-    }
+    const { data } = await this.api.get('/market/candles', { params });
+    return data as CandlesResponse;
   };
 
   public marketSearchByFigi = async (figi: string): Promise<MarketInstrumentListResponse> => {
-    try {
-      const { data } = await this.api.get('/market/search/by-figi', {
-        params: {
-          figi: figi.toUpperCase(),
-        }
-      });
-      return data as MarketInstrumentListResponse;
-    } catch (e) {
-      console.error(e.message);
-    }
+    const { data } = await this.api.get('/market/search/by-figi', {
+      params: {
+        figi: figi.toUpperCase(),
+      }
+    });
+    return data as MarketInstrumentListResponse;
   };
 
   public marketSearchByTicker = async (ticker: string): Promise<MarketInstrumentListResponse> => {
-    try {
-      const { data } = await this.api.get('/market/search/by-ticker', {
-        params: {
-          ticker: ticker.toUpperCase(),
-        }
-      });
-      return data as MarketInstrumentListResponse;
-    } catch (e) {
-      console.error(e.message);
-    }
+    const { data } = await this.api.get('/market/search/by-ticker', {
+      params: {
+        ticker: ticker.toUpperCase(),
+      }
+    });
+    return data as MarketInstrumentListResponse;
   };
 
   // Operations: Получение информации по операциям
 
   public operations = async (params: OperationsParams): Promise<OperationsResponse> => {
-    try {
-      const { data } = await this.api.get('/operations', { params });
-      return data as OperationsResponse;
-    } catch (e) {
-      console.error(e.message);
-    }
+    const { data } = await this.api.get('/operations', { params });
+    return data as OperationsResponse;
   };
 
   // User: Получение информации по брокерским счетам
 
   public userAccounts = async (): Promise<UserAccountsResponse> => {
-    try {
-      const { data } = await this.api.get('/user/accounts');
-      return data as UserAccountsResponse;
-    } catch (e) {
-      console.error(e.message);
-    }
+    const { data } = await this.api.get('/user/accounts');
+    return data as UserAccountsResponse;
   };
 }
+
