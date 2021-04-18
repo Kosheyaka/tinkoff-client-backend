3bcad31cb625fffa937dcda3039f0d27c6cefbf6
Dmitry Volkov <koshey0@inbox.ru>
2021-04-18 08:41:34 +0300

    md5 function replaced by md5 npm package because of troubles with cyrillic strings

--- src/Utils/md5.spec.ts
+++ src/Utils/md5.spec.ts
@@ -1,7 +1,15 @@
-import { md5 } from './md5';
+import * as md5 from 'md5';
 
 describe('md5', () => {
-  it('Test 1', async () => {
-    expect(md5('hello')).toBe('5d41402abc4b2a76b9719d911017c592');
+  const cases: [string, string][] = [
+    ['hello', '5d41402abc4b2a76b9719d911017c592'],
+    ['test_string', '3474851a3410906697ec77337df7aae4'],
+    ['Кириллица', '1e5b03112eb72c8f27f2a3dd0cb2df01'],
+    ['998877', '05dc4be3550a5f2ec6bdb5e3a2fc5059'],
+    ['Words with spaces', '5fbe81a873e105bd40e13401eb01e2cc'],
+  ];
+
+  test.each(cases)('%s', (input, expected) => {
+    expect(md5(input)).toBe(expected);
   });
 });
