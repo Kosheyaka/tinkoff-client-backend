4b4d5c31df5a3551279d0c7ab9a8b0ed97758920
Dmitry Volkov <koshey0@inbox.ru>
2021-04-18 08:02:27 +0300

    turn off dev module for prod

--- src/main.module.ts
+++ src/main.module.ts
@@ -3,8 +3,11 @@ import { TestModule } from './Modules/Test/test.module';
 import { TinkoffModule } from './Modules/Tinkoff/tinkoff.module';
 import { UtilsModule } from './Utils/utils.module';
 
+const prodModules = [TinkoffModule, UtilsModule];
+const devModules = [...prodModules, TestModule];
+
 @Module({
-  imports: [TestModule, TinkoffModule, UtilsModule],
+  imports: process.env.NODE_ENV === 'prod' ? prodModules : devModules,
   controllers: [],
   providers: [],
 })
