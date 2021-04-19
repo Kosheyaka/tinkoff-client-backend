3ea3c56ff4d3c42b2c63769ed7d71b2a7b855fec
Dmitry Volkov <koshey0@inbox.ru>
2021-04-19 04:26:46 +0300

    md5 hashes as cache keys

--- src/Utils/cache.service.ts
+++ src/Utils/cache.service.ts
@@ -1,5 +1,6 @@
 import { Cache } from 'cache-manager';
 import { CACHE_MANAGER, Inject } from '@nestjs/common';
+import md5 from 'md5';
 
 export class CacheService {
   constructor(
@@ -7,11 +8,13 @@ export class CacheService {
     private cacheManager: Cache,
   ) {}
 
-  private userKey = (key: string, userId: number) => `${userId}_${key}`;
+  private globalKey = (key: string) => md5(key);
+  private userKey = (key: string, userId: number) =>
+    md5([key, userId].join('.'));
 
   public async getGlobal<T>(key: string): Promise<T | null> {
     try {
-      return this.cacheManager.get(key);
+      return this.cacheManager.get<T>(this.globalKey(key));
     } catch (e) {
       return null;
     }
@@ -19,14 +22,14 @@ export class CacheService {
 
   public async getForUser<T>(key: string, userId: number): Promise<T | null> {
     try {
-      return this.cacheManager.get(this.userKey(key, userId));
+      return this.cacheManager.get<T>(this.userKey(key, userId));
     } catch (e) {
       return null;
     }
   }
 
   public async setGlobal<T>(key: string, value: T, ttl = null): Promise<T> {
-    return this.cacheManager.set(key, value, ttl);
+    return this.cacheManager.set<T>(this.globalKey(key), value, ttl);
   }
 
   public async setForUser<T>(
@@ -35,14 +38,14 @@ export class CacheService {
     userId: number,
     ttl = null,
   ): Promise<T> {
-    return this.cacheManager.set(this.userKey(key, userId), value, ttl);
+    return this.cacheManager.set<T>(this.userKey(key, userId), value, ttl);
   }
 
   public async delete(key: string) {
     return this.cacheManager.del(key);
   }
 
-  public clear(): Promise<void> {
+  public async clear() {
     return this.cacheManager.reset();
   }
 }
